8f767921b3adfe3660ef4450bb65f9784fe08290
ElMoumnaouiKaoutar <kaoutarelmoumnaoui2001@gmail.com>
2023-11-16 14:59:39 +0100

    Partie frontend de la page d'inscription

---
+++ smart_health_front/src/components/Inscription.js
@@ -0,0 +1,121 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { HiOutlineArrowCircleRight } from "react-icons/hi";
+
+const fields = [
+  {
+    label: "First Name",
+    type: "text",
+    placeholder: "entrer your first name",
+    required: true,
+    gridCols: 1,
+  },
+  {
+    label: "Last Name",
+    type: "text",
+    placeholder: "entrer your last name",
+    required: true,
+    gridCols: 1,
+  },
+  {
+    label: "Email",
+    type: "email",
+    placeholder: "entrer your email",
+    required: true,
+    gridCols: 2,
+  },
+  {
+    label: "Phone",
+    type: "tel",
+    placeholder: "Entrer your phone unmber",
+    required: true,
+    gridCols: 2,
+  },
+  {
+    label: "Address",
+    type: "text",
+    placeholder: "Entrer your address",
+    required: true,
+    gridCols: 2,
+  },
+  {
+    label: "Password",
+    type: "password",
+    placeholder: "Enter your password",
+    required: true,
+    gridCols: 1,
+  },
+  {
+    label: "Confirm Password",
+    type: "password",
+    placeholder: "Confirm your password",
+    required: true,
+    gridCols: 1,
+  },
+];
+
+export default function Inscription() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // You can perform further actions with the form data here
+  };
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="lg:w-7/12 pb-10 pt-5 w-full p-4 flex flex-wrap justify-center shadow-2xl my-20 rounded-md mx-auto">
+          <div className="pb-5">
+            <h1 className="text-3xl font-bold">Register Form</h1>
+          </div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col justify-start items-center w-full m-auto"
+          >
+            <div className="grid grid-cols-1 mb-6 md:grid-cols-2 gap-3 w-full">
+              {fields.map((field, index) => (
+                <div
+                  key={index}
+                  className={`text-left flex flex-col gap-2 w-full ${
+                    field.gridCols === 2 ? "md:col-span-2" : ""
+                  }`}
+                >
+                  <label className="font-semibold">{field.label}</label>
+                  <input
+                    {...register(field.label.toLowerCase(), {
+                      required: field.required,
+                    })}
+                    className={`border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 md:mb-0 focus:border-red-500 ${
+                      field.gridCols === 2 ? "md:w-full" : ""
+                    }`}
+                    type={field.type}
+                    placeholder={field.placeholder}
+                  />
+                  {errors[field.label.toLowerCase()] && (
+                    <span>This field is required</span>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="w-full text-left">
+              <button
+                type="submit"
+                className="flex justify-center items-center gap-2 w-full py-3 px-4 bg-red-500 text-white text-md font-bold border border-red-500 rounded-md ease-in-out duration-150 shadow-slate-600 hover:bg-white hover:text-red-500 lg:m-0 md:px-6"
+                title="Confirm Order"
+              >
+                <span>Register</span>
+                <HiOutlineArrowCircleRight size={20} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
